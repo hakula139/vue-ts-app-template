4ca0580cac64bbb60bb7c0374570c55851fec0f5
hakula139 <1517242376@qq.com>
2021-11-24 17:20:24 +0800

    feat(router): introduce vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,10 @@
 <template>
-  <HelloWorld />
+  <router-view v-slot="{ Component }">
+    <transition
+      name="fade"
+      mode="out-in"
+    >
+      <component :is="Component" />
+    </transition>
+  </router-view>
 </template>
-
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-import { HelloWorld } from '@/components';
-
-export default defineComponent({
-  components: {
-    HelloWorld,
-  },
-});
-</script>
