052be8307caeac8d58ccdea8b345c2b3a54499c0
Hakula Chen <i@hakula.xyz>
2021-12-19 23:12:06 +0800

    fix: minor fix

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 
 import App from '@/App.vue';
 import router from '@/router';
-import 'ant-design-vue/dist/antd.less';
 import '@/assets/css/index.css';
 
 const app = createApp(App);
