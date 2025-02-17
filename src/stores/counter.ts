31d2f5f699af70926ca653abebdfe8178d9424ee
Hakula Chen <i@hakula.xyz>
2025-02-17 17:11:46 +0800

    feat(counter): make store persistent

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -2,9 +2,13 @@ import { ref } from 'vue';
 import { defineStore } from 'pinia';
 
 export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0);
+  const localCount = localStorage.getItem('count');
+  const initialCount = localCount ? Number(JSON.parse(localCount)) : 0;
+  const count = ref(initialCount);
+
   const increment = () => {
     count.value++;
+    localStorage.setItem('count', JSON.stringify(count.value));
   };
 
   return {
